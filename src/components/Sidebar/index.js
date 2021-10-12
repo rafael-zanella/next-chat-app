065499e98f9cd48178ccb7da0e70c2f17df51ade
Rafael Scotti Zanella <rafaels.zanella@gmail.com>
2021-10-12 01:46:53 -0300

    chore: configure directories and dependecies

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Sidebar = () => {
+  return(
+    <Container>
+        Sidebar
+    </Container>
+  )
+}
+
+export { Sidebar };
+
+
+const Container = styled.div`
+
+`;
