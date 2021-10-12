c5003fb09ded6738b9241d3379100d7d0be8b51e
Rafael Scotti Zanella <rafaels.zanella@gmail.com>
2021-10-12 19:25:59 -0300

    feat(Login): add loading

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 import Head from 'next/head';
 import { Button } from "@material-ui/core";
+import { signInWithGoogle } from "../../firebase";
 
 const Login = () => {
+
+  const signIn = () => {
+    signInWithGoogle().catch(alert);
+  }
 
   return (
     <Container>
@@ -12,8 +17,14 @@ const Login = () => {
 
       <LoginContainer>
         <Logo src='http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png' />
-        <Button variant="outlined">Sign in with Google</Button>
+        <Button
+          variant="outlined"
+          onClick={signIn}
+        >
+          Sign in with Google
+        </Button>
       </LoginContainer>
+
     </Container>
   )
 
